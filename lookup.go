b82c1d6694bc865aba0eecf81d209c2956141ae4
David R. <50171348+da-rod@users.noreply.github.com>
2026-03-12 10:26:32 +0100

    switch to net/netip

--- lookup.go
+++ lookup.go
@@ -1,24 +1,52 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+	"net/netip"
+	"sort"
+)
 
-func lookup(input []string) results {
+type result struct {
+	addr netip.Addr
+	ptr  []string
+}
+
+func (r result) print() {
+	fmt.Println(r.addr, r.ptr)
+}
+
+type results []result
+
+func (r results) print() {
+	sort.Slice(r, func(i, j int) bool {
+		return r[i].addr.Compare(r[j].addr) < 0
+	})
+	for _, res := range r {
+		if *zero && len(res.ptr) == 0 {
+			continue
+		}
+		res.print()
+	}
+}
+
+func lookup(input []netip.Addr) results {
 	size := len(input)
 	ch := make(chan result, size)
 
 	for _, ipaddr := range input {
 		ip := ipaddr
 		go func() {
-			rdns, _ := net.LookupAddr(ip)
+			rdns, _ := net.LookupAddr(ip.String())
 			ch <- result{
-				addr: net.ParseIP(ip),
+				addr: ip,
 				ptr:  rdns,
 			}
 		}()
 	}
 
 	res := make(results, 0, size)
-	for i := 0; i < size; i++ {
+	for range size {
 		r := <-ch
 		res = append(res, r)
 	}
--- main.go
+++ main.go
@@ -2,31 +2,23 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"flag"
 	"fmt"
-	"net"
 	"os"
-	"sort"
+	"strings"
 )
-
-type result struct {
-	addr net.IP
-	ptr  []string
-}
-
-type results []result
 
 var zero *bool
 
 func init() {
 	flag.Usage = func() {
-		fmt.Fprint(flag.CommandLine.Output(), "Reverse DNS lookup tool.\n\n")
-		fmt.Fprint(flag.CommandLine.Output(), "Takes an IP address or CIDR as argument (or from stdin (one per line)).\n")
-		fmt.Fprint(flag.CommandLine.Output(), "(Also accepts \"truncated\" CIDRs x.y.z which are treated as x.y.z.0/24.)\n\n")
-		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s <IP or CIDR>\n", os.Args[0])
+		out := flag.CommandLine.Output()
+		fmt.Fprint(out, "Reverse DNS lookup tool.\n\n")
+		fmt.Fprint(out, "Takes an IP address or CIDR as argument (or from stdin (one per line)).\n")
+		fmt.Fprint(out, "(Also accepts \"truncated\" CIDRs x.y.z which are treated as x.y.z.0/24.)\n\n")
+		fmt.Fprintf(out, "Usage: %s <IP or CIDR>\n", os.Args[0])
 		flag.PrintDefaults()
-		fmt.Fprintf(flag.CommandLine.Output(), "\nExamples:\n  - %s 140.82.112.32/29\n  - %s < ips.txt\n  - cat ips.txt | %s\n", os.Args[0], os.Args[0], os.Args[0])
+		fmt.Fprintf(out, "\nExamples:\n  - %s 140.82.112.32/29\n  - %s < ips.txt\n  - cat ips.txt | %s\n", os.Args[0], os.Args[0], os.Args[0])
 	}
 	zero = flag.Bool("z", false, "Filter out IP addresses without reverse DNS")
 }
@@ -35,14 +27,13 @@ func main() {
 	flag.Parse()
 
 	// Parse input for lookup
-	var entries []string
-	input := flag.Arg(0)
-	switch input {
+	var entries []any
+
+	switch input := flag.Arg(0); input {
 	case "", "-":
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
-			in := scanner.Text()
-			if ok, val := isValid(in); ok {
+			if val, ok := isValid(scanner.Text()); ok {
 				entries = append(entries, val)
 			}
 		}
@@ -51,23 +42,36 @@ func main() {
 			os.Exit(2)
 		}
 	default:
-		if ok, val := isValid(input); ok {
+		if val, ok := isValid(input); ok {
 			entries = append(entries, val)
 		}
 	}
 
-	out := lookup(buildHostsList(entries))
-	printResults(out)
+	ips := getHosts(entries)
+	res := lookup(ips)
+	res.print()
 }
 
-func printResults(results results) {
-	sort.Slice(results, func(i, j int) bool {
-		return bytes.Compare(results[i].addr, results[j].addr) < 0
-	})
-	for _, res := range results {
-		if *zero && len(res.ptr) == 0 {
-			continue
-		}
-		fmt.Println(res.addr, res.ptr)
+func isValid(input string) (any, bool) {
+	// remove empty/comment lines / trim inline comments
+	input, _, _ = strings.Cut(input, "#")
+	input = strings.TrimSpace(input)
+	// refang defanged strings
+	input = strings.ReplaceAll(input, "[.]", ".")
+	// accept truncated cidr x.y.z, consider it as x.y.z.0/24
+	octets := strings.Split(input, ".")
+	switch {
+	case len(octets) == 4 && octets[len(octets)-1] == "":
+		input = strings.TrimSuffix(input, ".")
+		fallthrough
+	case len(octets) == 3:
+		input = input + ".0/24"
 	}
+	if ip, ok := isIP(input); ok {
+		return ip, ok
+	}
+	if cidr, ok := isCIDR(input); ok {
+		return cidr, ok
+	}
+	return nil, false
 }
